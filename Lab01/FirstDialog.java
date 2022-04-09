0ecd9c620889542011c8d64e3c3333d8b27797e6
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2022-04-09 19:53:21 +0700

    fix text in firstdialog

--- Lab01/FirstDialog.java
+++ Lab01/FirstDialog.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class FirstDialog {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello?");
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
         System.exit(0);
     }
 }
