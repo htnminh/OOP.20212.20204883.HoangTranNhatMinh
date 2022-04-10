973e1f8a04b2f801f9656da2dc6834a29568c483
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2022-04-10 19:23:18 +0700

    done array

--- Lab01/ArrayProject workspace/src/ArrayProgram.java
+++ Lab01/ArrayProject workspace/src/ArrayProgram.java
@@ -1,4 +1,14 @@
+import java.util.Arrays;
 
 public class ArrayProgram {
-
+	public static void main(String[] args) {
+		int[] array = {1789, 2035, 1899, 1456, 2013};
+		
+		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
+		
+		System.out.println("Sum = " + Arrays.stream(array).sum());
+		System.out.println("Avg = "
+				+ Arrays.stream(array).average().getAsDouble());
+	}
 }
