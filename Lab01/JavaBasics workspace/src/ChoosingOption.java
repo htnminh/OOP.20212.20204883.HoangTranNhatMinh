f31d11c10b8518e09c04694c24af54797b72e99f
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2022-04-10 14:05:26 +0700

    minor fix

--- Lab01/JavaBasics workspace/src/ChoosingOption.java
+++ Lab01/JavaBasics workspace/src/ChoosingOption.java
@@ -5,7 +5,7 @@ public class ChoosingOption {
         int option = JOptionPane.showConfirmDialog(null,
         				"Do you want to change to the first class ticket?");
         JOptionPane.showMessageDialog(null, "You've chosen: " 
-        		+ (option == JOptionPane.YES_OPTION ? "Yes" : "No"));
+        		+ (option == JOptionPane.YES_OPTION ? "Yes" : "No"));     
         System.exit(0);
     }
 }
