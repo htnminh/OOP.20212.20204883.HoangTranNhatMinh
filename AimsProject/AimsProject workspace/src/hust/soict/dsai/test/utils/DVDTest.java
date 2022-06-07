c8f67a5bc329c1baf65eaa63452a363e5ab6660e
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2022-06-07 20:52:53 +0700

    delete DVDUtils

--- AimsProject/AimsProject workspace/src/hust/soict/dsai/test/utils/DVDTest.java
+++ AimsProject/AimsProject workspace/src/hust/soict/dsai/test/utils/DVDTest.java
@@ -1,6 +1,7 @@
 package hust.soict.dsai.test.utils;
+
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
-import hust.soict.dsai.aims.utils.DVDUtils;
+import hust.soict.dsai.aims.utils.MediaUtils;
 
 public class DVDTest {
 
@@ -16,24 +17,24 @@ public class DVDTest {
 		DigitalVideoDisc dvd5 = new DigitalVideoDisc(
 				"EEEE", "eeee", "eeee", 5, 3.3f);
 		
-		System.out.println(DVDUtils.compareByCost(dvd1, dvd2));
-		System.out.println(DVDUtils.compareByTitle(dvd5, dvd3));
+		System.out.println(MediaUtils.compareByCost(dvd1, dvd2));
+		System.out.println(MediaUtils.compareByTitle(dvd5, dvd3));
 		
 		DigitalVideoDisc dvdArray[] =
 				new DigitalVideoDisc[] {dvd1, dvd2, dvd3, dvd4, dvd5};
 		
 		
-		System.out.println("Sort by cost:");
-		DigitalVideoDisc[] sorted = DVDUtils.sortByCost(dvdArray);
-		for (DigitalVideoDisc dvd: sorted) {
-			System.out.println(dvd);
-		}
-		
-		System.out.println("Sort by title:");
-		sorted = DVDUtils.sortByTitle(dvdArray);
-		for (DigitalVideoDisc dvd: sorted) {
-			System.out.println(dvd);
-		}
+//		System.out.println("Sort by cost:");
+//		DigitalVideoDisc[] sorted = MediaUtils.sortByCostDescendingTitle(dvdArray);
+//		for (DigitalVideoDisc dvd: sorted) {
+//			System.out.println(dvd);
+//		}
+//		
+//		System.out.println("Sort by title:");
+//		sorted = MediaUtils.sortByTitle(dvdArray);
+//		for (DigitalVideoDisc dvd: sorted) {
+//			System.out.println(dvd);
+//		}
 
 	}
 }
