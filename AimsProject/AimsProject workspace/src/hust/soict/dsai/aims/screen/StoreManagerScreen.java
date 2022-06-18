26b450887aba7155a3c9837dbcf3005579cda3de
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2022-06-18 11:36:22 +0700

    created storemanagerscreen

---
+++ AimsProject/AimsProject workspace/src/hust/soict/dsai/aims/screen/StoreManagerScreen.java
@@ -0,0 +1,84 @@
+package hust.soict.dsai.aims.screen;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+import hust.soict.dsai.aims.media.Media;
+import hust.soict.dsai.aims.store.Store;
+
+public class StoreManagerScreen {
+	private Store store;
+	
+	public static void main(String[] args) {
+		new StoreManagerScreen();
+	}
+	
+	JPanel createNorth() {
+		JPanel north = new JPanel();
+		north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
+		north.add(createMenuBar());
+		// TODO
+		
+		return north;
+	}
+	
+	JMenuBar createMenuBar() {
+		JMenu menu = new JMenu("Options");
+		
+		menu.add(new JMenuItem("View store"));
+		
+		JMenu smUpdateStore = new JMenu("Update Store");
+		smUpdateStore.add(new JMenuItem("Add Book"));
+		smUpdateStore.add(new JMenuItem("Add CD"));
+		smUpdateStore.add(new JMenuItem("Add DVD"));
+		menu.add(smUpdateStore);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+		menuBar.add(menu);
+		
+		return menuBar;
+	}
+	
+	JPanel createHeader() {
+		JPanel header = new JPanel();
+		header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+		JLabel title = new JLabel("AIMS");
+		title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 50));
+		title.setForeground(Color.CYAN);
+		
+		header.add(Box.createRigidArea(new Dimension(10, 10)));
+		header.add(title);
+		header.add(Box.createHorizontalGlue());
+		header.add(Box.createRigidArea(new Dimension(10, 10)));
+		
+		return header;
+	}
+	
+	JPanel createCenter() {
+		JPanel center = new JPanel();
+		
+		center.setLayout(new GridLayout(3, 3, 2, 2));
+		
+		ArrayList<Media> mediaInStore = store.getItemsInStore();
+		for (int i = 0; i < Math.min(9, mediaInStore.size()); i++) {
+			// TODO 
+		}
+		
+		return center;
+	}
+	
+	
+}
