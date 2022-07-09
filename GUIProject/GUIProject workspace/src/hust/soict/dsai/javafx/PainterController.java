9a2281eab9b6a250a45775749742cd490a1cdd43
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2022-07-09 20:56:40 +0700

    done eraser

--- GUIProject/GUIProject workspace/src/hust/soict/dsai/javafx/PainterController.java
+++ GUIProject/GUIProject workspace/src/hust/soict/dsai/javafx/PainterController.java
@@ -2,6 +2,8 @@ package hust.soict.dsai.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -12,11 +14,23 @@ public class PainterController {
 
     @FXML
     private Pane drawingAreaPane;
+    @FXML
+    private RadioButton penRadioButton;
+    @FXML
+    private RadioButton eraserRadioButton;
+    @FXML
+    private ToggleGroup penEraserGroup;
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-    	Circle newCircle = new Circle(
-    			event.getX(), event.getY(), 4, Color.BLACK);
+    	Circle newCircle;
+    	if ((RadioButton)(penEraserGroup.getSelectedToggle()) == penRadioButton) {
+			newCircle = new Circle(
+	    			event.getX(), event.getY(), 4, Color.BLACK);
+		} else {
+			newCircle = new Circle(
+	    			event.getX(), event.getY(), 4, Color.WHITE);
+		}
     	drawingAreaPane.getChildren().add(newCircle);
     }
 
